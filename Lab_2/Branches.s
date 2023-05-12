384dc1e01fce53279ee2e48d52dd0f54e1ab98f7
Adam Lam <116122970+adamlam2304@users.noreply.github.com>
2023-05-12 10:13:35 -0700

    Update Branches.s

--- Lab_2/Branches.s
+++ Lab_2/Branches.s
@@ -6,63 +6,72 @@ Z: .word 0
 
 finalMsg: 	.ascii "final Z value"
 
-.text
 
-main: 
+
+.text
 lw a0, A
 lw a1, B
 lw a2, C
 lw a3, Z
 
-slt t0, a0, a1      # t0 gets 1 if A<B
-addi t1, zero, 5    # t1 = 5
-slt t1, t1, a2      # t1 gets 1 if 5<C
-bne t1, t0, elseif  # if t1 not equal to t0 go to elseif
-addi a3, zero, 1
+#if - else statement
+
+blt a0, a1, if1lessthan
+j if2
+
+if1lessthan:
+li t0,5
+bgt a2, t0, if1True
+j if2
+
+if1true:
+li t2,1
 j switch
 
-elseif:
+if2:
+bgt a0,a1, if2True
+li t0,1
+li t1,7
+add t0, t0, a2 
 
-slt t2, a1, a0  # t2 gets 1 if B<A
-addi t3, a2, 1  # t3 = C+1
-addi t4, zero, -7  # t4 = 7
+beq t0,t1,if2True
+li t2,3
 
-bne t3, t4, else
-
-beq t2, zero, else  # if t2 = 0 branch
-addi a3, zero, 2    # z gets 2
 j switch
 
-else:
-addi a3, zero, 3  # z gets 3
+if2True:
+li t2,2
+
+#switch statement
 
 switch:
+li t0,1
+beq t2, t0, case1
 
-addi s2, zero, -1 
-addi s3, zero, -2
-addi s4, zero, -3
+li t0,2
+beq t2, t0, case2
 
-beq a3, s2, break 
-beq a3, s3, break
-beq a3, s4, break
+li t0,3
+beq t2, t0, case3
 
-break:
+li t0,0
+sw t0,Z,t1
 
-sw a3, Z, t1
-sw a0, A, t1
-sw a1, B, t1
-sw a2, C, t1
+j end
+
+case1:
+li t0,-1
+j end
+
+case2:
+li t0,-2
+j end
+
+case3:
+li t0,-3
 
 
-
-li  a7, 4 			# print prompt string
-la  a0, finalMsg
-    ecall
-    
-li  a7, 1 			# print integer
-lw  a0, Z 
-ecall
-    
-li a7,10
-lw t1, Z
+end:
+sw t0,Z,t1
+li a7, 10	
 ecall
