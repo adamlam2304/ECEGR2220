3e7c98294e95450ed0844826ed9c8f898a1dca21
Adam Lam <116122970+adamlam2304@users.noreply.github.com>
2023-06-09 12:23:47 -0700

    Update ProcElements.vhd

--- Lab_6/ProcElements.vhd
+++ Lab_6/ProcElements.vhd
@@ -17,6 +17,13 @@ end entity BusMux2to1;
 architecture selection of BusMux2to1 is
 begin
 -- Add your code here
+	
+	with selector select
+	
+		Result <= In0 when '0',
+			  In1 when '1',
+			  (others=>'Z') when others;
+	
 end architecture selection;
 
 --------------------------------------------------------------------------------
@@ -41,8 +48,60 @@ entity Control is
 end Control;
 
 architecture Boss of Control is
+	
+	--add signals
+	
+	signal funct10code: std_logic_vector (10 downto 0);
+	signal opclock: std_logic_vector (7 downto 0);
+
 begin
 -- Add your code here
+	
+	
+	
+	funct10code <= funct7 & funct3 & opcode(4);
+	opclock <= clk & opcode;
+
+	with funct3 select
+		Branch <= "01" when "000",
+			  "10" when "001",
+			  "00" when others;
+
+	with opcode select
+		MemRead <= '1' when "0000011",
+			   '0' when others;
+
+	with opcode select
+		MemtoReg <= '1' when "0000011",
+			    '0' when others;
+
+	with funct10code select
+		ALUCtrl <= "00000" when "00000000001",
+			   "10000" when "10000000001",
+			   "00001" when "00000001111",
+			   "00010" when "00000001101",
+			   "00011" when "00000000011",
+			   "10011" when "00000001011",
+			   "00100" when "----------0",
+			   (others=>'Z') when others;
+
+	with opclock select
+		MemWrite <= '1' when "10100011",
+			    '0' when others;
+
+	with opcode select
+		ALUSrc <= '0' when "0110011",
+			  '1' when others;
+
+	with opcode select
+		RegWrite <= '0' when "1100011",
+			    '0' when "0100011",
+			    '1' when others;
+
+	with opcode select
+		ImmGen <= "00" when "0110011",
+			  "01" when others;
+
 
 end Boss;
 
@@ -63,5 +122,13 @@ architecture executive of ProgramCounter is
 begin
 -- Add your code here
 
+	process (Clock)
+	begin
+		if (Reset = '1') then
+			PCout <= x"00400000";
+		elsif rising_edge(Clock) then
+			PCout <= PCin;
+		end if;
+	end process;
 end executive;
 --------------------------------------------------------------------------------
